abb2c5ebc00fd1a959531a8d4aa2a4a02ff25f9d
javier.torres <javier.torres@cecar.edu.co>
2021-10-29 19:20:14 -0500

    proyectofinal

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 from flask import Flask
-from flask import render_template, request, flash, redirect, url_for,session,jsonify
+from flask import render_template, request, flash, redirect, url_for,session,session
 import flask
 from flaskext.mysql import MySQL
 from pymysql import cursors
 import os
+
 
 
 app= Flask(__name__)
@@ -23,59 +24,196 @@ def index():
 #MENU USUARIOS
 @app.route('/indexUser')
 def indexUser():
-    return render_template('user/indexUser.html')
+    
+    
+     
+
+    if 'user' in session:
+        return render_template('user/indexUser.html')
+    else:
+        return "no tienes permiso a esta ruta"
 @app.route('/searchFlight')
 def searchFlight():
-    return render_template('user/searchFlight.html')
+    if 'user' in session:
+        return render_template('user/searchFlight.html')
+    else:
+        return "no tienes permiso a esta ruta"
 @app.route('/rateFlight')
 def rateFlight():
-    return render_template('user/rateFlight.html')
+    if 'user' in session:
+        return render_template('user/rateFlight.html')
+    else:
+        return "no tienes permiso a esta ruta"
+    
   
 
 
 #MENU ADMIN
 @app.route('/indexAdmin')
 def indexAdmin():
-    return render_template('admin/indexAdmin.html')
+    if 'user1' in session:
+        return render_template('admin/indexAdmin.html')
+    else:
+        return "no tienes permiso a esta ruta"   
 @app.route('/userAdmin')
 def userAdmin():
-    return render_template('admin/userAdmin.html')  
+    if 'user1' in session:
+        return render_template('admin/userAdmin.html')  
+    else:
+        return "no tienes permiso a esta ruta" 
+#ELIMINAR USUARIOS
 @app.route('/userDelete')
 def userDelete():
-    return render_template('admin/userDelete.html')   
+    sql = "SELECT * FROM `usuarios`;"
+    conn=mysql.connect()
+    cursor=conn.cursor()
+    cursor.execute(sql)
+    usuarios=cursor.fetchall()
+    if 'user1' in session:
+        return render_template('admin/userDelete.html',usuarios=usuarios) 
+    else:
+        return "no tienes permiso a esta ruta"
+@app.route('/destroyUser/<int:id>')
+def destroyUser(id): 
+    conn=mysql.connect()
+    cursor=conn.cursor()
+
+    cursor.execute("DELETE FROM usuarios WHERE id=%s",(id))
+    conn.commit()
+    return redirect('/userDelete')
+#EDITAR USUARIOS      
 @app.route('/userEdit')
 def userEdit():
-    return render_template('admin/userEdit.html')    
+    if 'user1' in session:
+        return render_template('admin/userEdit.html')
+    else:
+        return "no tienes permiso a esta ruta"         
 #/////////////////////////////////////////////////////////////
 @app.route('/pilotAdmin')
 def pilotAdmin():
-    return render_template('admin/pilotAdmin.html')
+    if 'user1' in session:
+            return render_template('admin/pilotAdmin.html')
+    else:
+        return "no tienes permiso a esta ruta"     
 @app.route('/pilotAdd')
 def pilotAdd():
-    return render_template('admin/pilotAdd.html')
+    if 'user1' in session:
+            return render_template('admin/pilotAdd.html')
+    else:
+        return "no tienes permiso a esta ruta"    
 @app.route('/pilotDelete')
 def pilotDelete():
-    return render_template('admin/pilotDelete.html')
+
+    sql = "SELECT * FROM `pilotos`;"
+    conn=mysql.connect()
+    cursor=conn.cursor()
+    cursor.execute(sql)
+    pilotos=cursor.fetchall()
+
+    if 'user1' in session:
+            return render_template('admin/pilotDelete.html', pilotos=pilotos)
+    else:
+        return "no tienes permiso a esta ruta"   
+#ELIMINAR PILOTO
+@app.route('/destroyPilot/<int:id>')
+def destroyPilot(id): 
+    conn=mysql.connect()
+    cursor=conn.cursor()
+
+    cursor.execute("DELETE FROM pilotos WHERE id=%s",(id))
+    conn.commit()
+    return redirect('/pilotDelete')
+
+#EDITAR PILOTO
 @app.route('/pilotEdit')
 def pilotEdit():
-    return render_template('admin/pilotEdit.html')
+    
+    sql = "SELECT * FROM `pilotos`;"
+    conn=mysql.connect()
+    cursor=conn.cursor()
+    cursor.execute(sql)
+    pilotos=cursor.fetchall()
+
+    if 'user1' in session:
+            return render_template('admin/pilotEdit.html', pilotos=pilotos)
+    else:
+        return "no tienes permiso a esta ruta" 
+
+
+@app.route('/editPilot/<int:id>')
+def editPilot(id):
+    
+    conn=mysql.connect()
+    cursor=conn.cursor()
+    cursor.execute("SELECT * FROM pilotos WHERE id=%s",(id))
+    pilotos=cursor.fetchall()
+    conn.commit()
+    
+    return render_template('admin/editPilot.html',pilotos=pilotos)
+
+@app.route('/updatePiloto', methods=['POST'])
+def updatePiloto(): 
+    _identificacion=request.form['txtId']
+    _nombres=request.form['txtNombre']
+    _apellidos=request.form['txtApellido']
+    _genero=request.form['genero']
+    _correo=request.form['txtCorreo']
+    _nacimiento=request.form['txtFecha']
+    _usuario=request.form['txtUsuario']
+    _contrasena=request.form['txtContrasena']
+    id=request.form['txtVar']
+
+    sql = "UPDATE pilotos SET identificacion=%s, nombres=%s, apellidos=%s, genero=%s, correo=%s, nacimiento=%s, usuario=%s, contrasena=%s WHERE id=%s;"
+    
+    datos=(_identificacion,_nombres,_apellidos,_genero,_correo,_nacimiento,_usuario,_contrasena,id)
+
+    conn=mysql.connect()
+    cursor=conn.cursor()
+    cursor.execute(sql,datos)
+    conn.commit()
+    return redirect('/pilotAdmin')
+    
 #////////////////////////////////////////////////////////////
 @app.route('/flightAdmin')
 def flightAdmin():
-    return render_template('admin/flightAdmin.html')
-# @app.route('/flightAdd')
-# def flightAdd():
-#     return render_template('admin/flightAdd.html')
+    if 'user1' in session:
+            return render_template('admin/flightAdmin.html')
+    else:
+        return "no tienes permiso a esta ruta"   
 @app.route('/flightDelete')
 def flightDelete():
-    return render_template('admin/flightDelete.html')
+    sql = "SELECT * FROM `vuelos`;"
+    conn=mysql.connect()
+    cursor=conn.cursor()
+    cursor.execute(sql)
+    vuelos=cursor.fetchall()
+    if 'user1' in session:
+            return render_template('admin/flightDelete.html',vuelos=vuelos)
+    else:
+        return "no tienes permiso a esta ruta"    
 @app.route('/flightEdit')
 def flightEdit():
-    return render_template('admin/flightEdit.html')
+    if 'user1' in session:
+           return render_template('admin/flightEdit.html')
+    else:
+        return "no tienes permiso a esta ruta"    
 #/////////////////////////////////////////////////////
 @app.route('/rateAdmin')
 def rateAdmin():
-    return render_template('admin/rateAdmin.html')
+
+    sql = "SELECT * FROM `sugerencias`;"
+    conn=mysql.connect()
+    cursor=conn.cursor()
+    cursor.execute(sql)
+    sugerencias=cursor.fetchall()
+    print(sugerencias)
+    if 'user1' in session:
+           return render_template('admin/rateAdmin.html',sugerencias=sugerencias)
+    else:
+        return "no tienes permiso a esta ruta"    
+    
+
+
 
 
 #LOGIN ADMIN
@@ -116,16 +254,36 @@ def flightAdd():
 @app.route('/reservaFlight')
 def reservaFlight():
 
-    sql = "SELECT * FROM `vuelos`;"
+    sql = "SELECT vuelos.id, lugares.id AS idOrigen, lugares.ciudad As Origen, vuelos.destino, destino.ciudad, pilotos.id, CONCAT(pilotos.nombres, ' ', pilotos.apellidos) AS piloto, vuelos.fecha FROM vuelos JOIN lugares ON vuelos.origen = lugares.id JOIN destino ON vuelos.destino = destino.id JOIN pilotos ON vuelos.piloto = pilotos.id;"
     conn=mysql.connect()
     cursor=conn.cursor()
     cursor.execute(sql)
     vuelos=cursor.fetchall()
 
+
     
 
+    if 'user' in session:
+        return render_template('user/reservaFlight.html',vuelos=vuelos)  
+    else:
+        return "no tienes permiso a esta ruta"
 
-    return render_template('user/reservaFlight.html',vuelos=vuelos)  
+#REGISTRO DE RESERVAS  EN LA BASE DE DATOS
+@app.route('/addReserva', methods=['POST'])
+def addReserva():
+    _lugar=request.form['lugarTxt']
+    _fechaTxt=request.form['fechaTxt']
+    _fechaRegresoTxt=request.form['fechaRegresoTxt']
+
+    sql = "INSERT INTO `reserva` (`id`,`lugar`, `estado`, `fecha`, `fecha_regreso`) VALUES (NULL,%s, NULL, %s, %s);"
+    
+    datos=(_lugar,_fechaTxt,_fechaRegresoTxt)
+
+    conn=mysql.connect()
+    cursor=conn.cursor()
+    cursor.execute(sql,datos)
+    conn.commit()
+    return render_template('user/reservaFlight.html')
 
 #REGISTRO DE PILOTOS EN LA BASE DE DATOS
 @app.route('/registroPiloto', methods=['POST'])
@@ -167,7 +325,8 @@ def storage():
     _usuario=request.form['txtUsuario']
     _contrasena=request.form['txtContrasena']
 
-    
+   
+
 
     sql = "INSERT INTO `usuarios` (`id`,`identificacion`, `nombres`, `apellidos`, `genero`, `correo`, `nacimiento`, `usuario`, `contrasena`) VALUES (NULL,%s, %s, %s, %s, %s, %s, %s, %s);"
     
@@ -230,6 +389,7 @@ def addVuelo():
 def ingresar():
     txtUsuario = request.form["txtUsuario"]
     txtContrasena = request.form["txtContrasena"]
+    session['user'] = txtUsuario
     sql = "SELECT * FROM `usuarios` WHERE usuario = %s;"
     datos = (txtUsuario)
     
@@ -239,7 +399,7 @@ def ingresar():
     rest=cursor.fetchall()
     
     
-   
+       
     if len(rest)!=0:
         if txtUsuario == rest[0][7] and txtContrasena == rest[0][8]:
             session["usuario"] = txtUsuario
@@ -252,12 +412,45 @@ def ingresar():
         return render_template("login/index.html")
 
 
+#LOGOUT DE USUARIOS
+@app.route("/logoutUser")
+def logoutUser():
+    session.clear()
+    return render_template("login/index.html")
 
 
 
+#LOGIN DE ADMIN
+@app.route('/admin_login', methods=['POST'])
+def ingresarAdminn():
+    txtUsuario = request.form["txtUsuario"]
+    txtContrasena = request.form["txtContrasena"]
+    session['user1'] = txtUsuario
+    sql = "SELECT * FROM `administrador` WHERE usuario = %s;"
+    datos = (txtUsuario)
+    
+    conn=mysql.connect()
+    cursor=conn.cursor()
+    cursor.execute(sql,datos)
+    rest=cursor.fetchall()
+    
+    
+       
+    if len(rest)!=0:
+        if txtUsuario == rest[0][1] and txtContrasena == rest[0][2]:
+            session["usuario"] = txtUsuario
+            return render_template("admin/indexAdmin.html")
+        else:
+            flash("Correo o contraseña incorrectos")
+            return render_template("admin/loginAdmin.html")
+    else:
+        flash("Correo o contraseña incorrectos")
+        return render_template("admin/loginAdmin.html")
 
-
-
+@app.route("/logoutAdmin")
+def logoutAdmin():
+    session.clear()
+    return render_template("admin/loginAdmin.html")
 
 if __name__=='__main__':
     app.run(debug=True)
